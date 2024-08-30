0343a7dcc7bf1d73e42ec1a30d0d51062a74fe88
youssef belkhiri <belkhiriyoussef33@gmail.com>
2024-08-30 01:10:23 +0100

    add meta binary search

--- Searching/binary_search.cpp
+++ Searching/binary_search.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int IterativeBinarySearch(int arr[] , int low , int high , int key){
@@ -17,14 +18,27 @@ int RecursiveBinarySearch(int arr[] , int low , int high , int key){
 	if(arr[mid] > key)return RecursiveBinarySearch(arr , low , mid - 1 , key);
 	else return RecursiveBinarySearch(arr , mid + 1 , high , key);
 }
+int MetainarySearch(int arr[] , int low , int high , int key){
+	int n = high;
+	while (n>0){
+		int mid = min(high - 1 , n / 2);
+		if(arr[mid] == key)return mid;
+		if(arr[mid] < key)n = (n - mid) / 2 ;
+		else n = mid / 2;
+	}
+	return -1;
+}
 int main(){
 	int arr[] ={15 , 16 , 17 , 18 , 56 , 91};
-	int key = 15;
+	int key = 752;
 	int high = sizeof(arr) / sizeof(arr[0]);
 	int index = IterativeBinarySearch(arr , 0 , high - 1 , key);
-	if(index == -1) cout<<"the element was not found";
+	if(index == -1) cout<<"the element was not found" << endl;
 	else cout << "the element is found at the index:" << index << endl; 
 	int indexR = RecursiveBinarySearch(arr , 0 , high - 1 , key);
-	if(index == -1) cout<<"the element was not found";
-	else cout << "the element is found at the index:" << index; 
+	if(indexR == -1) cout<<"the element was not found" << endl;
+	else cout << "the element is found at the index:" << index << endl;
+	int indexM = MetainarySearch(arr , 0 , high , key);
+	if(indexM == -1) cout<<"the element was not found";
+	else cout << "the element is found at the index:" << indexM; 
 }
